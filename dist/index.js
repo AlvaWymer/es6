301931070c42ce1ba6a080c45a7b3d4e44f65ff6
yangnaihua <yangnaihua.2008@163.com>
2017-10-18 14:35:34 +0200

    var let const

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,11 @@
 
 var a = 1;
 console.log(a);
+var b = 4;
+{
+    var _b = 5;
+}
+console.log(b);
+var c = 1;
+
+console.log(c);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 let a = 1; 
 console.log(a);
+var b = 4;
+{
+    let b = 5;
+}
+console.log(b);
+const c = 1;
+
+console.log(c);
